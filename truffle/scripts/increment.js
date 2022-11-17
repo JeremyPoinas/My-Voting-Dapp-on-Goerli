a92968fabd73b58137fbe0b4e0e0431bb60257a2
JeremyPoinas <poinas.jeremy@gmail.com>
2022-11-17 18:02:48 +0100

    update truffle-config + migrations + scripts

--- truffle/scripts/increment.js
+++ truffle/scripts/increment.js
@@ -1,23 +1,7 @@
-/*
-  Try `truffle exec scripts/increment.js`, you should `truffle migrate` first.
-
-  Learn more about Truffle external scripts: 
-  https://trufflesuite.com/docs/truffle/getting-started/writing-external-scripts
-*/
-
-const SimpleStorage = artifacts.require("SimpleStorage");
+const SimpleStorage = artifacts.require("Voting");
 
 module.exports = async function (callback) {
-  const deployed = await SimpleStorage.deployed();
-
-  const currentValue = (await deployed.read()).toNumber();
-  console.log(`Current SimpleStorage value: ${currentValue}`);
-
-  const { tx } = await deployed.write(currentValue + 1);
-  console.log(`Confirmed transaction ${tx}`);
-
-  const updatedValue = (await deployed.read()).toNumber();
-  console.log(`Updated SimpleStorage value: ${updatedValue}`);
-
+  const deployed = await Voting.deployed();
+  
   callback();
 };
